80f719faca062cdb89f391419488ced5105eac5d
Pericles <jpaf62@m.thm.de>
2023-10-20 13:20:55 +0200

    updated serial connection test

--- testSerialHub.py
+++ testSerialHub.py
@@ -3,12 +3,13 @@ import binascii
 
 if __name__ == "__main__":
 
-    with serial.Serial('/dev/ttyUSB0') as ser:
+    with serial.Serial('/dev/ttyUSB0', baudrate=9600, timeout=2) as ser:
         print(ser.name)
-        command = "0200300335"
-        print(f"Send command to hub: {command}")
-        print(f"encoded command: {command.encode('utf-8')}")
-        ser.write(command.encode('utf-8'))
+        command = b"0200300335"
+        # binascii.unhexlify("0200300335")
+        print(f"Send command to hub: {binascii.hexlify(command)}")
+        print(f"encoded command: {command}")
+        ser.write(command)
         ser.flush()
-        response = binascii.b2a_hex(ser.read(9))
+        response = binascii.hexlify(ser.read(9))
         print(f"Response Hub: {response}")
