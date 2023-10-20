ae235c0dd12e1c38c1c3ab82150916c6d0a81dcb
Pericles <jpaf62@m.thm.de>
2023-10-20 23:50:50 +0200

    updated test

--- testSerialHub.py
+++ testSerialHub.py
@@ -7,15 +7,17 @@ if __name__ == "__main__":
     ADDR = 0x00
     CMD = 0x30
     ETX = 0x03
-
-    SUM = hex(STX + ADDR + CMD + ETX)[:-2]
+    SUM = 0x35
+    #check_SUM = hex(STX + ADDR + CMD + ETX)[-2:]
+    status_command = "0200300335" #serial.to_bytes([STX, ADDR, CMD, ETX, SUM])
+    encoded_command = bytes.fromhex(status_command)
+    print(f"encoded_command: {encoded_command}")
+    sequences = encoded_command.hex("-").split("-")
+    # a = hex(int(0x35))
 
     with serial.Serial('/dev/ttyUSB0', baudrate=9600, timeout=2) as ser:
         print(ser.name)
-        command = b'\x02\x00\x30\x03\x35'
-        #print(f"Send command to hub: {binascii.hexlify(command)}")
-        print(f"encoded command: {command}")
-        ser.write(command)
+        ser.write(bytes.fromhex(status_command))
         ser.flush()
         response = ser.read(9)
         print(f"Response Hub: {response}")
