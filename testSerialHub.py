b758fced3e06b1054d11bd10aca49e2f355604af
Pericles <jpaf62@m.thm.de>
2023-10-25 19:18:53 +0200

    updated serial test

--- testSerialHub.py
+++ testSerialHub.py
@@ -30,30 +30,26 @@ def send_command2Hub(hub_command:str) -> list:
         raise ValueError("No response from Hub")
 
 
+
 if __name__ == "__main__":
 
     parser = argparse.ArgumentParser()
-    parser.add_argument("-s", "--status", action="store_true", help="send status command to Hub")
+    parser.add_argument("-o", "--open", action="store_true", help="send status command to Hub")
+    parser.add_argument("-pos", "--position", type=int, default=None, help="send status command to Hub")
     args = parser.parse_args()
+    position = args.position
+    if position:
+        assert isinstance(position, int) and position in range(1, 17), "door number must be a integer between 1 and 16"
+
+    STX = "02"
+    ADDR = "00" if not position else "0" + hex(position-1)[-1]
+    CMD = "31" if args.open else "30"
+    ETX = "03"
+    SUM = hex(int(STX, 16) + int(ADDR, 16) + int(CMD, 16) + int(ETX, 16))[-2:]
     status_command = "0200300335"
     open_command = "0200310336"
-    command = status_command if args.status else open_command
-    try:
-        status = send_command2Hub(command)
-    except (ValueError, AssertionError):
-        # try again
-        status = send_command2Hub(command)
-
-    print(''.join(status))
-
-    # test_response = '02003500000000033A'
-    # encoded_response = bytes.fromhex(test_response)
-    #
-    # sum = hex(0x02 + 0x00 + 0x31 + 0x03)
-    # seq = encoded_response.hex("-").split("-")
-    # length = len(seq)
-    # door_data = seq[3:5]
-    # str_data = ''.join(door_data)
+    command = STX + ADDR + CMD + ETX + SUM #status_command if args.status else open_command
+    print(f"command: {command}")
 
 
 
