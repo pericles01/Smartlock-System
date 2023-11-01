f697fcdacade544574eb740c31e0c7c7108a7b50
Pericles <jpaf62@m.thm.de>
2023-11-01 14:24:22 +0100

    updated login admin view

--- views/LoginAdminView.py
+++ views/LoginAdminView.py
@@ -5,16 +5,8 @@ import os
 
 
 class LoginAdminCard(MDCard):
-    monkey_hello = StringProperty()
-    monkey_hided_eyes = StringProperty()
-    monkey_closed_mouth = StringProperty()
+    login_image = StringProperty()
     def __init__(self, **kwargs):
         super(LoginAdminCard, self).__init__(**kwargs)
-        self.monkey_hello = os.path.normpath("./ressources/monkeywillkommen1.png")
-        self.monkey_hided_eyes = os.path.normpath("../ressources/monkeyhidedeyes.png")
-        self.monkey_closed_mouth = os.path.normpath("../ressources/monkeymouthclosed.png")
+        self.login_image = os.path.normpath("/home/peri/Desktop/Studium/Masterarbeit/Smartlock-System/ressources/monkeywillkommen1.png")
 
-
-class ClickableTextFieldRound(MDRelativeLayout):
-    text = StringProperty()
-    hint_text = StringProperty()
