3a8f173908e278b4cf8f53384bac52f1d9b64902
Pericles <jpaf62@m.thm.de>
2023-11-16 12:28:55 +0100

    added user membership

--- views/WelcomeView.py
+++ views/WelcomeView.py
@@ -1,24 +1,42 @@
-from kivy.properties import  StringProperty
+from kivy.properties import  StringProperty, ObjectProperty
+from kivy.uix.popup import Popup
+from kivy.uix.relativelayout import RelativeLayout
 from kivymd.uix.card import MDCard
-from kivymd.uix.button import MDFillRoundFlatIconButton
-from kivymd.color_definitions import colors
-from kivy.lang import Builder
-
-#Builder.load_file("views/WelcomeView.kv")
-
-class AdminLoginButton(MDFillRoundFlatIconButton):
-    def __init__(self, **kwargs):
-        super(AdminLoginButton, self).__init__(**kwargs)
-
-    #def login(self, widget): print(widget.text)
+from kivymd.uix.floatlayout import MDFloatLayout
 
 
 class LoginOptionCard(MDCard):
     text_option = StringProperty()
     icon_name = StringProperty()
+    press_callback = ObjectProperty()
     def __init__(self, **kwargs):
         super(LoginOptionCard, self).__init__(**kwargs)
         self.shadow_offset = (0, 1)
-    def show_func(self, name:str):
-        print(f"{name}")
-        print("------------")
+
+
+class WelcomeScreen(MDFloatLayout):
+    def __init__(self, **kwargs):
+        super(WelcomeScreen, self).__init__(**kwargs)
+
+        self.pin_dialog = Popup(title="Login with PIN", title_align="center", title_size="20sp", size_hint=(0.6, 0.5),
+                                     auto_dismiss=False)
+        self.pin_dialog_content = PinDialogContent()
+
+    def show_dialog(self, instance: str):
+        if instance.text_option == "Login with PIN":
+            self.pin_dialog_content.ids.exit_button.bind(on_press=self.pin_dialog.dismiss)
+            self.pin_dialog.content = self.pin_dialog_content
+            self.pin_dialog.open()
+        elif instance.text_option == "Login with RFID":
+            print(f"{str(instance.icon_name)}")
+            print("------------")
+        elif instance.text_option == "Login with QR Code":
+            print(f"{str(instance.icon_name)}")
+            print("------------")
+        else:
+            print(f"{str(instance.icon_name)}")
+            print("------------")
+
+class PinDialogContent(RelativeLayout):
+    def __init__(self, **kwargs):
+        super(PinDialogContent, self).__init__(**kwargs)
